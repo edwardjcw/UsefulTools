ccc816c30b0be2c5f0f72cd947d9ea9442538c2e
edward-vite <edward-vite@DESKTOP-2VMFAU2>
2017-03-26 11:03:09 -0500

    Add tool to randomly select a subset of n number of items from an array

---
+++ UsefulTools/UsefulTools/UsefulTools.fs
@@ -0,0 +1,20 @@
+﻿namespace UsefulTools
+
+open System
+
+type Tools() = 
+    static let random = Random(0)
+
+    static member RandomSubsetBySize n (a : 'a array) = 
+        match a.Length with
+        | count when count <= n -> a
+        | count ->  
+            let randoms = 
+                Seq.initInfinite (fun _ -> random.Next(count))
+                |> Seq.distinct
+                |> Seq.take n
+                |> Seq.toArray
+            randoms 
+            |> Array.map (fun i -> a.[i]) 
+
+        
